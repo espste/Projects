b8c664a49701722396aabf2456d8f208ba65cee5
Espen <91163328+espste@users.noreply.github.com>
2022-04-25 22:27:55 +0200

    Projects

---
+++ TvGuide/index.js
@@ -0,0 +1,31 @@
+/* 
+    TV Guide (Challenge)
+    
+    The API call is now searching ALL shows
+        instead of "singlesearch"
+    
+    Write and implement a getShowHtml(show) function
+       as well as displayShows(shows)
+       to display all of the results inside of a 
+       container with a class of my-shows
+*/
+
+async function findShow(query) {
+    let response = await fetch(`https://api.tvmaze.com/search/shows?q=${query}`)
+    let data = await response.json()
+    return data
+}
+
+findShow("office").then(shows => {
+    let show = shows[0].show
+
+    document.body.innerHTML = `<div class="my-show">
+        <div class="my-show-title">
+            ${show.name}
+        </div>
+        
+        <div class="my-show-summary">
+            ${show.summary}
+        </div>
+    </div>`
+})
